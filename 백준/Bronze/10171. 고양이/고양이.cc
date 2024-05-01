9fb04cda578aa6471e83f6dcf943d2c707900a2f
egkals <112994365+egkals@users.noreply.github.com>
2024-05-01 19:37:28 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "\\    \/\\\n";					// 특수 문자 출력은 \다음에 쓰면 됨
+	cout << " \)  \( \'\)\n";
+	cout << "\(  \/  \)\n";
+	cout << " \\\(\_\_\)\|\n";
+
+	return 0;
+}
