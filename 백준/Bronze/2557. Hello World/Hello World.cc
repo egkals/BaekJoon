7c670904774e42417ad2224329c0499239c9bf77
egkals <112994365+egkals@users.noreply.github.com>
2024-05-01 19:19:54 +0900

    [Bronze V] Title: Hello World, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;	// namespace = 변수, 함수 이름을 사용하는 공간으로 소속을 나타내는 역할
+						// std라는 것을 소속으로 사용하겠다는 뜻으로 입출력 함수 등의 앞에 안써도 저절로 사용 가능
+
+int main()
+{
+	cout << "Hello World!";		// cout(씨아웃) = 출력 함수, Console Output(콘솔 출력)
+	return 0;
+}
