a5b7bfef69c277102d3ca403831928afeb4b42fc
egkals <112994365+egkals@users.noreply.github.com>
2024-05-08 21:22:13 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -2,19 +2,19 @@
 
 using namespace std;
 
-int newmath(int a, int b)
+long long newmath(long long a, long long b)
 {
 	return (a + b) * (a - b);
 }
 
 int main()
 {
-	int a, b;
+	long long a, b;
 
 	while (1) {
 		cin >> a >> b;
 
-		if (a >= 1 && a <= 1000 && b <= 1000)
+		if (a >= 1 && b <= 100000)
 			break;
 		else
 			cout << "다시 입력하세요.";
